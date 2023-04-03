64ef5d46b8b7425802b24d87a6b8f7db7a5cf793
shelra90 <shelra90@yahoo.com>
2023-04-03 01:21:24 -0400

    fixed error in products.js

--- frontend/src/products.js
+++ frontend/src/products.js
@@ -4,7 +4,7 @@ const products = [
         name: 'Airpods Wireless Bluetooth Headphones',
         image: '/pics/airpods.jpg',
         description:
-            'Bluetooth technology lets you connect it with compatible devices wirelessly High-quality AAC audio offers immersive listening experience Built-in microphone allows you to take calls while working'.
+            'Bluetooth technology lets you connect it with compatible devices wirelessly High-quality AAC audio offers immersive listening experience Built-in microphone allows you to take calls while working',
         brand: 'Apple',
         category: 'Electronics',
         price: 89.99,
@@ -30,7 +30,7 @@ const products = [
     {
         _id: '3',
         name: 'Cannon EOS 80D DSLR Camera',
-        image: '/pics/camera.jpg'
+        image: '/pics/camera.jpg',
         description: 'Characterized by versatile imaging specs, the Canon EOS 80D further clarifies itself using a pair of robust focusing systems and an intuitive design',
         brand: 'Cannon',
         category: 'Electronics',
@@ -55,7 +55,7 @@ const products = [
     },
 
     {
-        _id: '5'
+        _id: '5',
         name: 'Logitech G-Series Gaming Mouse',
         image: '/pics/mouse.jpg',
         description: 'Get a better handle on your games with this Logitecth LIGHTSYNC gaming mouse. The six programmable buttons allow customization for a smooth playing experience',
