0bc0660421f046a4ed22f67b06a7e305101f27a1
shelra90 <shelra90@yahoo.com>
2023-04-03 02:30:43 -0400

    added ratings and ability to switch to product pg

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,27 @@
 import react from 'react'
 import {Container} from 'react-bootstrap'
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import Footer from './components/Footer';
 import Header from './components/Header';
 import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
+
 
 const App = () => {
   return (
    <>
+    <Router>
     <Header />
       <main>
         <Container>
-    <HomeScreen />
-    </Container>
-    </main>
+            <Routes>
+              <Route path="/" element={<HomeScreen />} exact />
+              <Route path="/product/:id" element={<ProductScreen />} />
+            </Routes>    
+        </Container>
+      </main>
     <Footer />
+    </Router>
    </>
   );
 }
