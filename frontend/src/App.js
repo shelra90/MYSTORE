320724a58e2c780d63d2eadb076b705b8ed1aecb
shelra90 <shelra90@yahoo.com>
2023-04-02 22:58:52 -0400

    created components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,13 @@
+import react from 'react'
+import Footer from './components/Footer';
+import Header from './components/Header';
 
-
-function App() {
+const App = () => {
   return (
    <>
+    <Header />
     <h1>Welcome to My Store!</h1>
+    <Footer />
    </>
   );
 }
